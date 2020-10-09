bede386d0b6207abe96ba6a47236283e199aa0d6
st0dgy <solokaty9@gmail.com>
2020-10-09 09:26:16 +0300

    first master commit

---
+++ laab2.cpp
@@ -0,0 +1,33 @@
+﻿// Lab_02.cpp 
+// <Соловей Катерина >
+// Лабораторна робота № 2.
+// Лінійні програми. 
+// Варіант 18 
+
+#include <iostream> 
+#include <cmath> 
+
+using namespace std;
+
+int main()
+{
+    double a;  // вхідний параметр    
+
+
+    double z1; // результат обчислення 1-го виразу 
+    double z2; // результат обчислення 2-го виразу 
+
+    cout << "a = ";
+    cin >> a;
+
+
+    z1 = (((a + 2) / sqrt(2 * a) - (a / (sqrt(2 * a) + 2)) + (2 / (a - sqrt(2 * a)))) * ((sqrt(a) - sqrt(2)) / (a + 2)));
+    z2 = 1 / (sqrt(a) + sqrt(2));
+
+    cout << endl;
+    cout << "z1 = " << z1 << endl;
+    cout << "z2 = " << z2 << endl;
+
+    cin.get();
+    return 0;
+}
